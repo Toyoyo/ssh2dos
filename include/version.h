56466b1cc0456b1624d54b6b3d544b5adc4c9de7
Toyoyo Toyo <toyoyo@wirefull.org>
2026-02-07 15:22:48 +0100

    Also update version for the same reason

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
 
 /* SSH2DOS version */
 
-#define SSH_VERSION	"0.2.1+SHA256.1"
+#define SSH_VERSION	"0.2.1+SHA256.1+ed25519+curve25519-sha256"
 #define AUTHOR_1 "SSH2DOS by Daniel Nagy (2000-2006) - http://sshdos.sourceforge.net/"
 #define AUTHOR_2 "SHA256 patch by Antti Takala - https://github.com/AnttiTakala/SSH2DOS"
 #define AUTHOR_3 "ed25519+curve25519-sha256 patch by Toyoyo - https://github.com/Toyoyo/ssh2dos"
